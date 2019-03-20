2eec4e62c94395f7fcbc557dd8cb13cedc332369
yaowenqiang <yaowenqiang111@163.com>
2019-03-21 02:27:27 +0800

    add demo

---
+++ c_primer_plus/demo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+    int dogs;
+    printf("How many dogs do you have? \n");
+    scanf("%d", &dogs);
+    printf("So You have  %d dog(s)!\n", dogs);
+}
---
+++ chd/enum_demo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+    enum Jar_type {
+      CUP,
+      PINT,
+      QUART,
+      HALF_GALLON, 
+      GALLON
+    };
+
+    enum Jar_type milk_jug, gas_can, medicine_bottle;
+    /*
+    enum Jar_type {
+      CUP,
+      PINT,
+      QUART,
+      HALF_GALLON, 
+      GALLON
+    } milk_jug, gas_can, medicine_bottle;
+    */
+
+    /*
+    enum Jar_type {
+      CUP = 8,
+      PINT = 16,
+      QUART = 32,
+      HALF_GALLON, = 64 
+      GALLON = 128
+    } milk_jug, gas_can, medicine_bottle;
+    */
+    
+
+}
