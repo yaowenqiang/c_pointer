f02a2aeb3af9bb637d9fde9c6f21f81fc6631c97
yaowenqiang <yaowenqiang111@163.com>
2019-03-22 01:53:09 +0800

    add fathom to feet or feet to fathom example

---
+++ c_primer_plus/fathons.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int feet, fathoms;
+    fathoms = 2;
+    feet = 6 * fathoms;
+    printf("There are %d feet in %d fathoms !\n", feet, fathoms);
+    return 0;
+}
+
