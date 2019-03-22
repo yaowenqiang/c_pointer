6db4c553cd4c49c47bbb2ae22a0f5e4e4c6c992d
yaowenqiang <yaowenqiang111@163.com>
2019-03-23 04:15:27 +0800

    add two functions demo

---
+++ c_primer_plus/tow_func.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void bulter(void);
+int main(void)
+{
+    printf("I will summon the bulter function.\n");
+    butler();
+    printf("Yes, Bring me some tea and writable DVDs.\n");
+    return 0;
+}
+void butler(void)
+{
+    printf("You rang, sir?\n");
+}
