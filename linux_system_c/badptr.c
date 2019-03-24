680bb59a916893f42592e21cc572a71985c30003
yaowenqiang <yaowenqiang111@163.com>
2019-03-25 00:15:14 +0800

    add assert demo

---
+++ linux_system_c/badptr.c
@@ -0,0 +1,20 @@
+/*
+ * badptr.c - Testing assert
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main(void)
+{
+    FILE *fp;
+    fp = fopen("foo_bar","w"); /* This should work */
+    assert(fp);
+    fclose(fp);
+
+    fp = fopen("bar_baz","r"); /* This should fail */
+    assert(fp); 
+    fclose(fp); /* Should never get here */
+
+    exit(EXIT_FAILURE);
+}
