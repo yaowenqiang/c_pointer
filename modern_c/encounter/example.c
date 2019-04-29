a631a79c707d3598c426580a5dddd1b5c91a677c
yaowenqiang <yaowenqiang111@163.com>
2019-04-30 01:41:56 +0800

    add bool usage example

--- modern_c/encounter/example.c
+++ modern_c/encounter/example.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(int argc, char* argv[argc+1]) {
     puts("Hello_World!");
     if (argc > 1) {
-        while (1) {
+        while (true) {
             puts("some programs nerver stop");
         }
     } else {
