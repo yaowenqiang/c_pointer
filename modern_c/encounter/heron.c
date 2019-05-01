b4a3549ab499b0428e2f78c015755a03fc21d2bb
yaowenqiang <yaowenqiang111@163.com>
2019-05-01 15:54:45 +0800

    add heron c show  how  fabs works

---
+++ modern_c/encounter/heron.c
@@ -0,0 +1,21 @@
+/*
+ * heron.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* lower and upper iteration limits centered around 1.0 */
+static double const eps1m01 = 1.0 - 0x1P - 01;
+static double const eps1p01 = 1.0 + 0x1P - 01;
+static double const eps1m24 = 1.0 - 0x1P - 24;
+static double const eps1p24 = 1.0 + 0x1P - 24;
+
+int int main(int argc, char* *argv[argc +1])
+{
+    for (int i = 0; i < argc; ++i) {
+              
+    } 
+    
+    return 0;
+}
