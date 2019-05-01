e88a16f442a07ba6dc3c42ac98b47752e5e4729d
yaowenqiang <yaowenqiang111@163.com>
2019-05-01 16:17:16 +0800

    ninish the code ,fix some typo

--- modern_c/encounter/heron.c
+++ modern_c/encounter/heron.c
@@ -1,27 +1,42 @@
 /*
  * heron.c
+ * ./heron 0.07 5 6E*23
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 /* lower and upper iteration limits centered around 1.0 */
-static double const eps1m01 = 1.0 - 0x1P - 01;
-static double const eps1p01 = 1.0 + 0x1P - 01;
-static double const eps1m24 = 1.0 - 0x1P - 24;
-static double const eps1p24 = 1.0 + 0x1P - 24;
+static double const eps1m01 = 1.0 - 0x1P-01;
+static double const eps1p01 = 1.0 + 0x1P-01;
+static double const eps1m24 = 1.0 - 0x1P-24;
+static double const eps1p24 = 1.0 + 0x1P-24;
 
-int int main(int argc, char* argv[argc+1])
+int main(int argc, char* argv[argc+1])
 {
-    for (int i = 0; i < argc; ++i) {
+    for (int i = 1; i < argc; ++i) {
        double const a = strtod(argv[i], 0); 
        double x = 1.0;
        for(;;) {
            double prod = a * x;
-           if (prod < epsm01) {
+           if (prod < eps1m01) {
                x *= 2.0;
-           } else if (eps1p01 )
+           } else if (eps1p01 < prod ) {
+               x *= 0.5;
+           } else {
+               break;
+           }
        }
+       for(;;) {
+           double prod = a * x;
+           if ((prod < eps1m24) || (eps1p24 < prod)) {
+               x *= (2.0 - prod);
+           } else {
+               break;
+           }
+
+       }
+       printf("heron: a = %.5e, \tx=%.5e, \t a*x=%.12f\n", a, x, a*x);
               
     } 
     
