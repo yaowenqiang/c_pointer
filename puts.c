9079499c392dfe48b8f85ad6632d135b7fa715e1
yaowenqiang <yaowenqiang111@163.com>
2019-05-05 21:30:30 +0800

    add puts demo

---
+++ puts.c
@@ -0,0 +1,16 @@
+/*
+ * puts.c 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    puts("first line\n"
+        "another line\n"
+        "first and "
+        "part of thethird line\n"
+            );
+    puts("a" "b");
+}
