fc3a4495169b4800c71f67f9d20db2b76a2f0505
yaowenqiang <yaowenqiang111@163.com>
2019-05-26 23:51:53 +0800

    add more  scanf demo

--- c_primer_plus/input.c
+++ c_primer_plus/input.c
@@ -9,11 +9,18 @@ int main(void)
     int age;
     float assets;
     char pet[30];
+    char job[30];
+    char address[30];
+    
 
-    printf("Enter your age, assets, and vavorite pet.\n");
+    printf("Enter your age, assets, and favorite pet.\n");
     scanf("%d %f", &age, &assets);
     scanf("%s", pet);
     printf("%d $%.2f %s\n", age, assets, pet);
+    printf("Enter your job, address.\n");
+    scanf("%s", job);
+    scanf(" %s", address);
+    printf("YOU job is %s and your address is %s\n", job, address);
     return 0;
 }
 
