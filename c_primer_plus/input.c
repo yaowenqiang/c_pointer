f498137a40372ac61a0db81f2f089f839bf4f93e
yaowenqiang <yaowenqiang111@163.com>
2019-05-26 23:39:49 +0800

    add scanf input demo

---
+++ c_primer_plus/input.c
@@ -0,0 +1,19 @@
+/*
+ * input.c - 什么情况下使用&
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int age;
+    float assets;
+    char pet[30];
+
+    printf("Enter your age, assets, and vavorite pet.\n");
+    scanf("%d %f", &age, &assets);
+    scanf("%s", pet);
+    printf("%d $%.2f %s\n", age, assets, pet);
+    return 0;
+}
+
