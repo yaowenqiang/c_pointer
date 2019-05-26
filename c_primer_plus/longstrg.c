9c5c2e25100b85ae49a847d6dee5dc1643016f35
yaowenqiang <yaowenqiang111@163.com>
2019-05-26 22:59:41 +0800

    add print long strings demo

---
+++ c_primer_plus/longstrg.c
@@ -0,0 +1,20 @@
+/*
+ * longstrg.c - 打印长的字符串
+ */
+
+#include <stdio.h>
+int main(void)
+{
+    printf("Here's one way to print a ");
+    printf("long string\n");
+    printf("Here's another way to print a \
+long string\n");
+    printf("Here's another way to print a \
+            long string\n");
+    printf("Here's the newest way to print a "
+            "long string.\n");
+    printf("Hello, young lovers, wherever you are.\n");
+    printf("Hello, young "    "lovers"    ", wherever you are.\n");
+    printf("Hello, young lovers, "
+            "wherever you are.\n");
+}
