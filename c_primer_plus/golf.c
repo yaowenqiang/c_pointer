1a5c7d8c81ee2df08b6f7ebbd7df82010becfdea
yaowenqiang <yaowenqiang111@163.com>
2019-05-29 01:45:53 +0800

    add multi variables assignment demo

---
+++ c_primer_plus/golf.c
@@ -0,0 +1,12 @@
+/*
+ * golf.c - 高尔夫锦标赛记分卡
+ */
+
+#include <stdio.h>
+int main(void)
+{
+    int jane, tarzan, cheeta;
+    cheeta = tarzan = jane = 68;
+    printf("                  cheeta tarzan jane\n");
+    printf("First round score %4d %6d %6d\n", cheeta, tarzan, jane);
+}
