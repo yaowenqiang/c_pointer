1273a0edf4e7cee750923b1043dba4b4161bfd82
yaowenqiang <yaowenqiang111@163.com>
2019-05-31 02:30:09 +0800

    add more divide operation demo

--- c_primer_plus/divide.c
+++ c_primer_plus/divide.c
@@ -7,9 +7,13 @@
 int main(void)
 {
     printf("integer devision: 5/4 is %d \n", 5/4);
+    printf("integer devision: -5/4 is %d \n", -5/4);
+    printf("integer devision: -3/4 is %d \n", -3/4);
     printf("integer devision: 6/3 is %d \n", 6/3);
     printf("integer devision: 7/4 is %d \n", 7/4);
     printf("floating devision: 7./4. is %1.2f \n", 7./4.);
     printf("mixed devision: 7.4/4 is %1.2f \n", 7./4);
+    printf("mixed devision: 0.0/4 is %1.2f \n", 0.0/4);
+    printf("mixed devision: 0.0/0.0 is %1.2f \n", 0.0/0.0);
     return 0;
 }
