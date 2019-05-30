e3dfcca9af72970845a0331dc09335bd5eb718df
yaowenqiang <yaowenqiang111@163.com>
2019-05-31 02:08:01 +0800

    add divide operation demo

---
+++ c_primer_plus/divide.c
@@ -0,0 +1,15 @@
+/*
+ * divide.c - 我们所知的除法
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("integer devision: 5/4 is %d \n", 5/4);
+    printf("integer devision: 6/3 is %d \n", 6/3);
+    printf("integer devision: 7/4 is %d \n", 7/4);
+    printf("floating devision: 7./4. is %1.2f \n", 7./4.);
+    printf("mixed devision: 7.4/4 is %1.2f \n", 7./4);
+    return 0;
+}
