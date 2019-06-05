21090fbc40fc532c666f65a25b0de2cf5a3ea70e
yaowenqiang <yaowenqiang111@163.com>
2019-06-06 03:32:58 +0800

    add function with argument demo

---
+++ c_primer_plus/pound.c
@@ -0,0 +1,25 @@
+/*
+ * pound.c - 定义带有一个参数的函数
+ */
+
+#include <stdio.h>
+
+void pound(int n);
+int main(void)
+{
+    int times = 5;
+    char ch = '!';
+    float f = 6.0;
+    pound(times);
+    pound(ch);
+    pound((int)f);
+    return 0;
+}
+
+void pound(int n)
+{
+    while (n-- > 0) {
+        printf("#");
+    }
+    printf("\n");
+}
