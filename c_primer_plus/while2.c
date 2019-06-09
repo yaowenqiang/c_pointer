6d069bb792d57decb5c61097d4aaae8c20520cbf
yaowenqiang <yaowenqiang111@163.com>
2019-06-09 12:17:21 +0800

    add bad while usage demo

---
+++ c_primer_plus/while2.c
@@ -0,0 +1,14 @@
+/*
+ * while2.c - 注意分号的使用
+ */
+
+#include <stdio.h>
+int main(void)
+{
+    int n = 0;
+    while (n++ <3);
+        printf("n is %d\n", n);
+
+    printf("That's all this program does.\n");
+    return 0;
+}
