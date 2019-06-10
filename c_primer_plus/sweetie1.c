6940a861758de434f82c24d12ac313ee6622e153
yaowenqiang <yaowenqiang111@163.com>
2019-06-11 00:54:02 +0800

    add a count loop demo

---
+++ c_primer_plus/sweetie1.c
@@ -0,0 +1,15 @@
+/*
+ * sweetie1.c - 一个计数循环
+ */
+
+#include <stdio.h>
+int main(void)
+{
+    
+    const int NUMBER = 20;
+    int count = 1;
+    while (count <= NUMBER) {
+        printf("Be my Valentine!\n");
+    }
+    return 0;
+}
