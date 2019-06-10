541d217f7ce2c16ba5b4b3c6d2d86ce075e9c13b
yaowenqiang <yaowenqiang111@163.com>
2019-06-11 01:03:50 +0800

    add for count loop demo

---
+++ c_primer_plus/sweetie2.c
@@ -0,0 +1,14 @@
+/*
+ * sweetie2.c - 一个使用for的计数循环
+ */
+
+#include <stdio.h>
+int main(void)
+{
+    const int NUMBER = 20;
+    int count;
+    for (count = 0; count  <=  NUMBER; count++) {
+        printf("Be my Valentine!\n");
+    }
+    return 0;
+}
