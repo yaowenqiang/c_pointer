95aeaf376d0123a02ad41987d26c18228927ffec
yaowenqiang <yaowenqiang111@163.com>
2019-06-15 21:29:01 +0800

    add function pointer.c

---
+++ function_pointer.c
@@ -0,0 +1,22 @@
+/*
+ * function_pointer.c 
+ */
+#include <stdio.h>
+//char* (*pf)(int*)
+//https://www.learn-c.org/en/Function_Pointers
+
+void someFunction(int arg)
+{
+    printf("This is someFunction being called and arg is: %d\n", arg);
+    printf("Whoops leaving the function now!\n");
+
+}
+int main(void)
+{
+    void (*pf)(int);
+    pf = someFunction;
+    printf("We're about to call someFunction() using a pointer!\n");
+    (pf)(5);
+    printf("Wow that was cool. Back to main now!\n\n");
+    return 0;
+}
