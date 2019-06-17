78a2fca83f1642253adc4c32941abc3bc2c2d80f
yaowenqiang <yaowenqiang111@163.com>
2019-06-18 00:50:34 +0800

    add putchar and getchar demo

---
+++ c_primer_plus/cypher1.c
@@ -0,0 +1,22 @@
+/*
+ * cypher1.c - 改变输入，只保留其中的空格.
+ */
+
+#include <stdio.h>
+#define SPACE ' '
+int main(void)
+{
+    char ch;
+    ch = getchar();
+    while (ch != '\n') 
+    {
+        if (ch == SPACE) {
+            putchar(ch);
+        } else {
+            putchar(ch + 1);
+        }
+        ch = getchar();
+    }
+    putchar(ch);
+    return 0;
+}
