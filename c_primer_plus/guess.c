46b72e66a737dd5e37503ee5a904c38dcddb268e
yaowenqiang <yaowenqiang111@163.com>
2019-06-25 01:47:16 +0800

    add guess.c

---
+++ c_primer_plus/guess.c
@@ -0,0 +1,28 @@
+/*
+ * guess.c - 一个低效且错误的猜数程序
+ */
+
+#include <stdio.h>
+int main(void)
+{
+    int guess = 1;
+    char response;
+    printf("Pick an interger from 1 to 100, I will try to guess ");
+    printf("it.\nRespond with a y if my guess is right and with ");
+    printf("\nan n if it is wrong.\n") ;
+    printf("Uh... is your number %d?\n", guess);
+    while ((response = getchar()) != 'y') {
+        printf("Well, then ,is it %d?\n", ++guess);
+        if (response == 'n') {
+            printf("Well, then, is it %d\n", ++guess);
+        } else {
+            printf("Sorry, I understand only y or n.\n");
+        }
+        //while(getchar() != '\n') {
+        while(response != '\n') {
+            continue;
+        }
+    }
+    printf("I knew I could do it!\n");
+    return 0;
+}
