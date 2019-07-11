d65b580b6b41d8a4532f31fbcf799a3a6dcbfac4
yaowenqiang <yaowenqiang111@163.com>
2019-07-11 23:16:16 +0800

    fix typo

--- c_primer_plus/pnt_add.c
+++ c_primer_plus/pnt_add.c
@@ -7,15 +7,15 @@
 int main(void)
 {
     short dates[SIZE];
-    short * pt1;
+    short * ptl;
     short index;
     double bills[SIZE];
     double * ptf;
-    pt1 = dates;
-    pt2 = bills;
+    ptl = dates;
+    ptf = bills;
     printf("%23s %15s \n", "short", "double");
-    for (int index = 0; index < SIZE; index++) {
-        printf("pointers + %d: %10p %10p\n", index, pti + index );
+    for (index = 0; index < SIZE; index++) {
+        printf("pointers + %d: %10p %10p\n", index, ptl + index, ptf + index );
     }
 
     return 0;
