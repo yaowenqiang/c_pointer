cb7ab83e1af9e3109d370556deded7629e620224
yaowenqiang <yaowenqiang111@163.com>
2019-07-27 21:06:59 +0800

    add fgets demo

--- c_primer_plus/fgets1.c
+++ c_primer_plus/fgets1.c
@@ -7,7 +7,7 @@
 int main(void)
 {
     char words[STLEN];
-    puts("Enter a stringk please");
+    puts("Enter a string please");
     fgets(words, STLEN, stdin);
     printf("Your string twice (puts(), then fputs())\n");
     puts(words);
@@ -17,6 +17,21 @@ int main(void)
     printf("Your string twice (puts(), then fputs())\n");
     puts(words);
     fputs(words, stdout);
+    int i = 0;
+    while(words[i]) {
+        printf("%d\n", words[i]);
+        i++;
+    }
+    printf("%d\n", '\0');
     puts("Done.");
+    char tmp[] = "a";
+    printf("%d\n", tmp[0]);
+    printf("%d\n", tmp[1]);
+    printf("%d\n", tmp[2]);
+    printf("%d\n", tmp[2]);
+
+    char * mystr = "h";
+    printf("%s\n", mystr);
+
     return 0;
 }
