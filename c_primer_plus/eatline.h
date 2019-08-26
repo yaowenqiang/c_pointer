d41880a3f45d5131be3507dd9c9f841f8c9c843a
yaowenqiang <yaowenqiang111@163.com>
2019-08-27 01:52:26 +0800

    add inline function demo

---
+++ c_primer_plus/eatline.h
@@ -0,0 +1,14 @@
+/*
+ * eatline.h
+ *
+ */
+
+#ifndef EATLINE_H_
+#define EATLINE_H_
+inline static void eatline()
+{
+    while (getchar()!='\n') {
+        continue;
+    }
+}
+#endif  
---
+++ c_primer_plus/eatline_demo.c
@@ -0,0 +1,11 @@
+/*
+ * eatline_demo.c 
+ */
+#include <stdio.h>
+#include "eatline.h"
+
+int main(void)
+{
+    eatline();
+    return 0;
+}
