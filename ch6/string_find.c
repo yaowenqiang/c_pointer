fc6ef798c810e7b75d7acd9fe1facc7f01053e66
yaowenqiang <yaowenqiang111@163.com>
2019-09-12 02:12:03 +0800

    add find_char function

---
+++ ch6/string_find.c
@@ -0,0 +1,34 @@
+/*
+ * string_find.c - 给定一个指向以NULL结尾的指针列表的指针，在列表中的字符串中查找一个特定的字符
+ */
+
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int find_char(char ** strings, char value);
+
+
+int main(void)
+{
+    char * str2[] = {"abc", "def"};
+    char c = 'c'; 
+    printf("find result: %d", find_char(str2, c));
+    return 0;
+}
+int find_char(char ** strings, char value)
+{
+    char * string; /* 我们当前正在查找的字符串 */
+    while ((string = *strings++) != NULL) {
+        /*
+         * 观察字符串中的每个字符， 看看它是不是我们需要查找的那个
+         */
+        while (*string != '\0') {
+            if (*string++ == value) {
+                return TRUE;
+            }
+        }
+    }
+    return FALSE;
+}
