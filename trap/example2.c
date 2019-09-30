24a1fcde4b3472922ff3bef7a225f797fec065f7
yaowenqiang <yaowenqiang111@163.com>
2019-09-30 21:54:15 +0800

    add function pointer trap demo

--- trap/example2.c
+++ trap/example2.c
@@ -3,9 +3,26 @@
  */
 
 #include <stdio.h>
+typedef void (*funcptr)();
+//(*(funcptr)0)();
+
 int main(void)
 {
     int a ,b = 0;
     printf("a++ + ++b=%d\n", a++ + ++b);
+
+    void (*fp)();
+    //(*fp)();
+    //*((*fp)());
+
+    //(*fp)();
+
+    //(void (*)())0;
+
+
+    (*(void (*)())0)();
     return 0;
+
+
+
 }
