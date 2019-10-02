c3231ded0fffc04412f8773373cae8f2c69cdf62
yaowenqiang <yaowenqiang111@163.com>
2019-10-02 23:27:58 +0800

    add binary_to_ascii function

--- ch13/mystery2.c
+++ ch13/mystery2.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 void mystery(int n);
+void binary_to_ascii(unsigned int value);
+void binary_to_ascii_v2(unsigned int value);
 int main(void)
 {
     mystery(1);
@@ -12,6 +14,8 @@ int main(void)
     printf("%c\n", "xyz" + 1);
     printf("%p\n", "xyz" + 1);
     printf("%c\n", ("xyz" + 1)[0]);
+    binary_to_ascii_v2(111);
+    binary_to_ascii(111);
     return 0;
 }
 
@@ -20,4 +24,22 @@ void mystery(int n)
     n += 5;
     n /= 10;
     printf("%s\n", "****************" +10 - n);
+}
+
+
+void binary_to_ascii(unsigned int value) 
+{
+    unsigned int quotient;
+    quotient = value / 10;
+    if (quotient != 0) {
+        binary_to_ascii(quotient);
+    }
+
+    putchar(value%10 + '0');
+}
+
+void binary_to_ascii_v2(unsigned int value) 
+{
+    
+    putchar("0123456789ABCDEF" [value % 16]);
 }
