df09b3c29f8980d1628dc0be6ab201ea6eea5125
yaowenqiang <yaowenqiang111@163.com>
2019-10-08 00:54:45 +0800

    add ctime, localtime, gmtime demo

--- ch16/clock.c
+++ ch16/clock.c
@@ -9,9 +9,16 @@ int main(void)
     //sleep(10);
     int ct2 = clock() / CLOCKS_PER_SEC;
     printf("%d\n", ct2 - ct1);
-    time_t *pt;
-    time(pt);
+    //time_t *pt;
+    time_t pt;
+    time(&pt);
 
-    printf("time = %ld\n", *pt);
+    printf("time = %ld\n", pt);
+
+    printf("%s\n",ctime(&pt));
+
+    struct tm * timeinfo;
+    timeinfo = localtime(&pt);
+    printf("today is %d\n", timeinfo->tm_mday);
     return 0;
 }
