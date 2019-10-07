d3d1ecfac26eaa03f177b99d1aa03c63029cb283
yaowenqiang <yaowenqiang111@163.com>
2019-10-07 23:46:58 +0800

    add get current time demo(cre dumped, TODO)

--- ch16/clock.c
+++ ch16/clock.c
@@ -6,8 +6,12 @@
 int main(void)
 {
     int ct1 = clock() / CLOCKS_PER_SEC;
-    sleep(10);
+    //sleep(10);
     int ct2 = clock() / CLOCKS_PER_SEC;
     printf("%d\n", ct2 - ct1);
+    time_t *pt;
+    time(pt);
+
+    printf("time = %ld\n", *pt);
     return 0;
 }
