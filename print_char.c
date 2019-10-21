751b9b84aa2acbfbb62f5084f3981aba686d93ac
Yaowenqiang <yaowenqiang111@163.com>
2019-10-21 21:52:30 +0800

    add more demo

--- print_char.c
+++ print_char.c
@@ -3,11 +3,29 @@ int main(void)
 {
     //char a = '';
     char a = '\0';
+    printf("%d\n", a);
+    char b[] = "a";
     printf("%c\n", a);
     printf("Enter a character: ");
-    scanf("%c", &a);
+    //scanf("%c", &a);
+    a = 'a';
     printf("%c\n", a);
     printf("%d\n", a);
+    printf("b is %s\n", b);
+    printf("before: b[0] is %c\n", b[0]);
+    b[0] = '\0';
+    printf("after: b[0] is %c\n", b[0]);
+    printf("%s\n", b);
+    char c[] = "";
+    printf("c is %s\n", c);
+    printf("c[0] is %c\n", c[0]);
+    printf("c[0] is %d\n", c[0]);
+
+    printf("c[1] is %c\n", c[1]);
+    printf("c[1] is %d\n", c[1]);
+
+    printf("c[2] is %c\n", c[2]);
+    printf("c[2] is %d\n", c[2]);
 
     return 0;
 }
